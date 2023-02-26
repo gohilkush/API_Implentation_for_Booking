1be5781312c8691c324d4e9dd212d72ea5dc8b0d
gohilkush <kush9.gohil@gmail.com>
2023-02-26 08:52:10 -0600

    adding al

---
+++ API_Implementation/routes/Rooms.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const router = express.Router();
+
+const getRooms = async (req, res, next) => {
+  try {
+    const data = fs.readFileSync(path.join(__dirname, './Room.json'));
+    const stats = JSON.parse(data);
+    const roomStats = stats.filter(room => room.Time === Number(req.params.Time));
+    if (!roomStats) {
+      const err = new Error('Player stats not found');
+      err.status = 404;
+      throw err;
+    }
+    res.json(roomStats);
+  } catch (e) {
+    next(e);
+  }
+};
+
+const ReserveRoom = async (req, res, next) => {
+  try {
+    const data = fs.readFileSync(path.join(__dirname, './Room.json'));
+    const stats = JSON.parse(data);
+    const roomStats1 = stats.find(allRoom => allRoom.Room === Number(req.params.Room) && allRoom.Time === Number(req.params.Time));
+    console.log(Number(req.params.Room), Number(req.params.Time)); 
+    if (roomStats1 != undefined) {
+      const err = new Error('error');
+      err.status = 404;
+      throw err;
+    }
+
+    const newStatsData = {
+      Room: Number(req.params.Room),
+      UserBooked: Number(req.params.UserBooked),
+      Time: Number(req.params.Time)
+    };
+
+    stats.push(newStatsData);
+    fs.writeFileSync(path.join(__dirname, './Room.json'), JSON.stringify(stats, null, "\t"));
+    res.status(200).json(newStatsData);
+  } catch (e) {
+    next(e);
+  }
+};
+
+const DeleteRoom = async (req, res, next) => {
+  try {
+    const data = fs.readFileSync(path.join(__dirname, './Room.json'));
+    const stats = JSON.parse(data);
+    const roomStats1 = stats.find(allRoom => allRoom.UserBooked === Number(req.params.UserBooked));
+    console.log(roomStats1);
+    if (roomStats1 == undefined) {
+      const err = new Error('Reservation Not Found');
+      err.status = 404;
+      throw err;
+    }
+
+    const newStatsData = {
+      Room: roomStats1.Room,
+		  UserBooked: null,
+		  Time: null
+    };
+
+    const newStats = stats.map(allRoom => {
+      if (allRoom.UserBooked === Number(req.params.UserBooked)) {
+        console.log("Done")
+        return newStatsData;
+      } else {
+        return allRoom;
+      }
+    });
+
+    fs.writeFileSync(path.join(__dirname, './Room.json'), JSON.stringify(newStats, null, "\t"));
+    res.status(200).json(newStatsData);
+  } catch (e) {
+    next(e);
+  }
+};
+
+const GetReservation = async (req, res, next) => {
+  try {
+    const data = fs.readFileSync(path.join(__dirname, './Room.json'));
+    const stats = JSON.parse(data);
+    const roomStats = stats.find(room => room.UserBooked === Number(req.params.UserBooked));
+    if (!roomStats) {
+      const err = new Error('No Reservations by user');
+      err.status = 404;
+      throw err;
+    }
+    res.json(roomStats);
+  } catch (e) {
+    next(e);
+  }
+};
+
+router
+  .route('/api/v1/Rooms/:Time')
+  .get(getRooms)
+
+router 
+  .route('/api/v1/Rooms/:Room/:Time/:UserBooked')
+  .put(ReserveRoom)
+
+router
+  .route('/api/v1/Rooms/:UserBooked')
+  .get(GetReservation)
+  .put(DeleteRoom)
+
+module.exports = router;
